edb8317c31e55f07ba4d6bd4d52bea1a903b6fed
Vladyslav Braiko <vladyslavbar@gmail.com>
2020-07-26 16:34:43 +0300

    Updates

---
+++ precache-manifest.dfa305ecffda48d411532384bbdffec4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b50c5d254c6eadd0da4cd478bd5cb2e4",
+    "url": "/star-wars-react-app/index.html"
+  },
+  {
+    "revision": "3543031647564a69a1c1",
+    "url": "/star-wars-react-app/static/css/main.489e18c7.chunk.css"
+  },
+  {
+    "revision": "0b901410abc65d9d37b4",
+    "url": "/star-wars-react-app/static/js/2.c1c4435a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/star-wars-react-app/static/js/2.c1c4435a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3543031647564a69a1c1",
+    "url": "/star-wars-react-app/static/js/main.c51e174c.chunk.js"
+  },
+  {
+    "revision": "729481abfd7ce9bad719",
+    "url": "/star-wars-react-app/static/js/runtime-main.70ab5a5c.js"
+  }
+]);
